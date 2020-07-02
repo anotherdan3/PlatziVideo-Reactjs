dbfd9ed3941a5fe020cfa5b39abc026d1746d8a1
Daniel Medina <medinalopez.danielandres@gmail.com>
2020-07-02 16:25:49 -0300

    Configuracion de Entorno de Trabajo(Webpack, Eslint, Babel,  Prettier)

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//Components
+import HelloWorld from './components/HelloWorld';
+
+const container = document.getElementById('root');
+ReactDOM.render(<HelloWorld />, container);
