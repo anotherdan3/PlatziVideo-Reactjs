d4d7cf3bd6cbd1a1ff7b0db56de40e9c1d3943f8
Daniel Medina <medinalopez.danielandres@gmail.com>
2020-07-05 15:39:48 -0300

    Conectando App a FakeApi

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -3,12 +3,12 @@ import '../assets/styles/components/CarouselItem.scss';
 import plusIcon from '../assets/images/plus-icon.png';
 import playIcon from '../assets/images/play-icon.png';
 
-const CarouselItem = () => (
+const CarouselItem = ({ cover, year, duration, title }) => (
   <div className="carousel-item">
     <img
       className="carousel-item__img"
-      src="https://images.pexels.com/photos/789822/pexels-photo-789822.jpeg?auto=format%2Ccompress&cs=tinysrgb&dpr=2&h=750&w=1260"
-      alt=""
+      src={cover}
+      alt="Movie Cover"
     />
     <div className="carousel-item__details">
       <div>
@@ -23,11 +23,9 @@ const CarouselItem = () => (
           alt="Plus Icon"
         />
       </div>
-      <p className="carousel-item__details--title">
-        Título descriptivo
-      </p>
+      <p className="carousel-item__details--title">{title}</p>
       <p className="carousel-item__details--subtitle">
-        2019 16+ 114 minutos
+        {`${year} ${duration} Minutes`}
       </p>
     </div>
   </div>
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 //Components
 import Header from '../components/Header';
 import Search from '../components/Search';
@@ -9,43 +9,67 @@ import Footer from '../components/Footer';
 //Styles
 import '../assets/styles/App.scss';
 
-const App = () => (
-  <div className="App">
-    <Header />
-    <Search />
-    <Categories title="Mi lista">
-      <Carousel>
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-      </Carousel>
-    </Categories>
-    <Categories title="Trending">
-      <Carousel>
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-      </Carousel>
-    </Categories>
-    <Categories title="New">
-      <Carousel>
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-      </Carousel>
-    </Categories>
-    <Footer />
-  </div>
-);
+const App = () => {
+  const [videos, setVideos] = useState({
+    mylist: [],
+    trends: [],
+    originals: [],
+  });
+
+  useEffect(() => {
+    fetch('http://localhost:3000/initialState')
+      .then((response) => response.json())
+      .then((data) => setVideos(data));
+  }, []);
+
+  return (
+    <div className="App">
+      <Header />
+      <Search />
+      {videos.mylist.length > 0 && (
+        <Categories title="My List">
+          <Carousel>
+            {videos.mylist.map((item) => (
+              <CarouselItem
+                key={item.id}
+                title={item.title}
+                year={item.year}
+                duration={item.duration}
+                cover={item.cover}
+              />
+            ))}
+          </Carousel>
+        </Categories>
+      )}
+      <Categories title="Trending">
+        <Carousel>
+          {videos.trends.map((item) => (
+            <CarouselItem
+              key={item.id}
+              title={item.title}
+              year={item.year}
+              duration={item.duration}
+              cover={item.cover}
+            />
+          ))}
+        </Carousel>
+      </Categories>
+      <Categories title="Originals">
+        <Carousel>
+          {videos.originals.map((item) => (
+            <CarouselItem
+              key={item.id}
+              title={item.title}
+              year={item.year}
+              duration={item.duration}
+              cover={item.cover}
+            />
+          ))}
+        </Carousel>
+      </Categories>
+      <Footer />
+    </div>
+  );
+};
 
 export default App;
