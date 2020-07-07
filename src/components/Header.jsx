8b0299b9c94fa4854b28feed8889889b8e7c6ed4
Daniel Medina <medinalopez.danielandres@gmail.com>
2020-07-07 17:20:00 -0300

    Agregando servicio de Gravatar al Proyecto

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,30 +1,46 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import '../assets/styles/components/Header.scss';
 import logoPlatzi from '../assets/images/logo-platzi-video-BW2.png';
 import userIcon from '../assets/images/user-icon.png';
+import gravatar from '../utils/gravatar';
 
-const Header = () => (
-  <header className="header">
-    <img
-      className="header__img"
-      src={logoPlatzi}
-      alt="Platzi Video"
-    />
-    <div className="header__menu">
-      <div className="header__menu--profile">
-        <img src={userIcon} alt="User Icon" />
-        <p>Perfil</p>
+const Header = (props) => {
+  const { user } = props;
+  const hasUser = Object.keys(user).length > 0;
+  return (
+    <header className="header">
+      <img
+        className="header__img"
+        src={logoPlatzi}
+        alt="Platzi Video"
+      />
+      <div className="header__menu">
+        <div className="header__menu--profile">
+          {hasUser ? (
+            <img src={gravatar(user.email)} alt="User Gravatar" />
+          ) : (
+            <img src={userIcon} alt="User Icon" />
+          )}
+          <p>Perfil</p>
+        </div>
+        <ul>
+          <li>
+            <a href="/">Cuenta</a>
+          </li>
+          <li>
+            <a href="/">Cerrar Sesión</a>
+          </li>
+        </ul>
       </div>
-      <ul>
-        <li>
-          <a href="/">Cuenta</a>
-        </li>
-        <li>
-          <a href="/">Cerrar Sesión</a>
-        </li>
-      </ul>
-    </div>
-  </header>
-);
+    </header>
+  );
+};
 
-export default Header;
+const mapStateToProps = (state) => {
+  return {
+    user: state.user,
+  };
+};
+
+export default connect(mapStateToProps, null)(Header);
